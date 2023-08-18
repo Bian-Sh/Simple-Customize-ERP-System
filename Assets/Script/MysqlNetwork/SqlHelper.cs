db6ea8bfdf19ebc20a70bc3a5f44e8360a1c087f
雨落随风 <bshsf@qq.com>
2023-08-18 21:10:12 +0800

    Update SqlHelper.cs
    删除未使用到的命名空间，避免打包报错

--- Assets/Script/MysqlNetwork/SqlHelper.cs
+++ Assets/Script/MysqlNetwork/SqlHelper.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Data.SqlTypes;
-using FullscreenEditor;
 using MySql.Data.MySqlClient;
 using UnityEngine;
 /*
